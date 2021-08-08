3446081470a1461435b5d1973fd1156c5acc3ccb
Lahari <vippagunta.lahari.met19@itbhu.ac.in>
2021-08-08 09:45:31 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,61 @@
+loadEvents();
+// load every event in the page
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+  document.getElementById('clear').addEventListener('click',clearList);
+  document.querySelector('ul').addEventListener('click',deleteOrTick);
+
+}
+// submit data function
+function submit(e){
+  e.preventDefault();
+  let taskList;
+  let input = document.querySelector('input');
+  if(input.value != '')
+    addTask(input.value);
+    console.log(jsdate.value);
+}
+
+// add tasks with date
+function addTask(task,date){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}, ${jsdate.value}</label>`;
+  ul.appendChild(li);
+  document.querySelector('.tasksBoard').style.display = 'block';
+}
+
+
+
+// clear the LIST
+function clearList(e){
+  let ul = document.querySelector('ul').innerHTML = '';
+}
+
+// deleteTick
+function deleteOrTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
+  }
+}
+
+// delete task
+function deleteTask(e){
+  let remove = e.target.parentNode;
+  let parentNode = remove.parentNode;
+  parentNode.removeChild(remove);
+}
+
+// tick a task
+function tickTask(e){
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.textDecoration = "line-through";
+    task.style.color = "#fff";
+  }else {
+    task.style.textDecoration = "none";
+    task.style.color = "#000000";
+  }
+}
